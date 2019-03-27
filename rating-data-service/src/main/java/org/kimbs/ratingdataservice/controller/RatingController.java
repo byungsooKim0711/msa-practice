cfd8673ce20fe21f13ae5c2355262556d5686b7a
kimbs <vlakd0711@naver.com>
2019-03-27 14:53:13 +0900

    rating-data-service

---
+++ rating-data-service/src/main/java/org/kimbs/ratingdataservice/controller/RatingController.java
@@ -0,0 +1,10 @@
+package org.kimbs.ratingdataservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rating")
+public class RatingController {
+
+}
