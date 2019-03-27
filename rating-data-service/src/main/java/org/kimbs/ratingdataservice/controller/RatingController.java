e2176ae5daa70bd3807646564ea015be83376b6b
kimbs <vlakd0711@naver.com>
2019-03-27 15:14:49 +0900

    rating data service

--- rating-data-service/src/main/java/org/kimbs/ratingdataservice/controller/RatingController.java
+++ rating-data-service/src/main/java/org/kimbs/ratingdataservice/controller/RatingController.java
@@ -1,5 +1,8 @@
 package org.kimbs.ratingdataservice.controller;
 
+import org.kimbs.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rating")
 public class RatingController {
 
+    @GetMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
 }
