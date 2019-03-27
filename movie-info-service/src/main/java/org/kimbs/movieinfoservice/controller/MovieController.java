d181a3a7e588deb49b02a7469c4fa6b75b91e8e6
kimbs <vlakd0711@naver.com>
2019-03-27 14:52:57 +0900

    movie-info-service

---
+++ movie-info-service/src/main/java/org/kimbs/movieinfoservice/controller/MovieController.java
@@ -0,0 +1,17 @@
+package org.kimbs.movieinfoservice.controller;
+
+import org.kimbs.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @GetMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable(name = "movieId") String movieId) {
+        return new Movie(movieId, "Test name");
+    }
+}
---
+++ movie-info-service/src/main/java/org/kimbs/movieinfoservice/models/Movie.java
@@ -0,0 +1,15 @@
+package org.kimbs.movieinfoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    private String movieId;
+
+    private String name;
+}
